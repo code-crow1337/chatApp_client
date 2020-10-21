b604cb3c36f8fb6befbd2e1576a743b468386e83
Josephine Een <josephine.een@gmail.com>
2020-10-21 16:54:47 +0200

    removed hardcoded localhost

--- src/containers/HomeScreen.tsx
+++ src/containers/HomeScreen.tsx
@@ -13,7 +13,7 @@ export function HomeScreen(props: any): React.ReactElement {
   useEffect(() => {
     const BACKEND = 'http://127.0.0.1:4000';
     const connectToSocket = async () => {
-      const socket = await socketIO.connect(BACKEND);
+      const socket = await socketIO.connect();
       openConnection(socket);
     };
     connectToSocket();
