0bd62ba1e8f50412106ae212ef1f8dec00126672
Josephine Een <josephine.een@gmail.com>
2020-10-22 09:54:20 +0200

    trying to get heroku to work

--- src/containers/ChatScreen.tsx
+++ src/containers/ChatScreen.tsx
@@ -31,7 +31,7 @@ export function ChatScreen(props: any): ReactElement {
     } else if(!connected && newUser.username !== "") {
       const connectToSocket = async () => {
         const BACKEND = 'https://code-crow1337-chat-app-server.herokuapp.com/';
-        const socket = await socketIO.connect(BACKEND);
+        const socket = await socketIO.connect(BACKEND, {secure: true});
 
         openConnection(socket);
         sendUsername(socket, newUser.username);
